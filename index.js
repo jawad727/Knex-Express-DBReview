ebc33d4f9743f667a4bf0a308d0b7b2226cc14fd
jawad727 <jawaddhussein@gmail.com>
2020-03-03 23:59:11 -0800

    get request to api appearing on page

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req, res) => {
+    res.send("API up and running")
+})
+
 server.listen(4000, () => {
     console.log("API UP AND RUNNING")
 })
